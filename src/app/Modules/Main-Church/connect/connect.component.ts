58b1ef031d9d9d3c99254a406777401a99a521af
acksa123 <acksa.varghese@logicplum.com>
2023-12-29 15:29:59 +0530

    Connect Animation added

--- src/app/Modules/Main-Church/connect/connect.component.ts
+++ src/app/Modules/Main-Church/connect/connect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-connect',
@@ -11,5 +11,22 @@ export class ConnectComponent implements OnInit {
 
   ngOnInit() {
   }
+translationLeft = 0;
+translationRight = 0;
+translationSpeed = 0.2;
 
+@HostListener('window:scroll', ['$event'])
+onScroll(event: any): void {
+  // Update translations based on scroll position and speed
+  this.translationLeft = window.scrollY * this.translationSpeed;
+  this.translationRight = -window.scrollY * this.translationSpeed; // Use a negative value for right translation
+}
+
+getTranslationLeft(): string {
+  return `translateX(${this.translationLeft}px)`;
+}
+
+getTranslationRight(): string {
+  return `translateX(${this.translationRight}px)`;
+}
 }
