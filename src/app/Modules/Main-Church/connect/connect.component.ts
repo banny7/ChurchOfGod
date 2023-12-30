0dc95e7c9c5bae8c87606f9f490f99cff8da7f80
Banny Biju <7bannybiju7@gmail.com>
2023-12-31 03:17:23 +0530

    Our Story Content added

--- src/app/Modules/Main-Church/connect/connect.component.ts
+++ src/app/Modules/Main-Church/connect/connect.component.ts
@@ -10,27 +10,53 @@ import { SharedModule } from '../../../Shared/Shared.module';
 })
 export class ConnectComponent implements OnInit {
 contents=[
-  { heading:'G101',
-  description:'This is your next step! Learn about the mission, values, and vision of Generation Church. This is a 1-time class that will answer any questions you may have, introduce you to key leaders, and give you a tour. Be ALL IN for your church and get plugged in now!'
+  {
+    heading: 'Discipleship',
+    description: 
+    'This is your next step! Learn about the mission, values, and vision of Gospel Centre. This is a 1-time class that will answer any questions you may have, introduce you to key leaders, and give you a tour. Be ALL IN for your church and get plugged in now!'
   },
   {
-    heading:'Kids',
-    description:'Kids are important to us at Generation Church! Find out about our ministries for little g’s (birth-kindergarten) and gKids (1st-5th grade).'
-  },{
-    heading:'Youth',
-    description:'GEN YTH is an energetic youth group for students in grades 6 through 12. Learn how Generation Church is raising our youth to love and follow Jesus.'
+    heading: 'Kids and Teens',
+    description: 'Kids are important to us at Gospel Centre! Find out about our ministries for little g’s (birth-kindergarten), gKids (1st-5th grade) and youth group for students in grades 6 through 12'
+  }, 
+  {
+    heading: 'Worship and Music',
+    description: `
+      Embrace the transformative power of worship in our community. Join our gatherings to experience the joy and connection that comes from praising God through music. Dive into the heart of our worship values and understand the significance of music in our spiritual journey. Come, let's lift our voices together!
+    `}
+  , 
+  {
+    heading: 'Prayer and Intercession',
+    description: "Engage in the spiritual discipline of prayer as we believe in its profound impact on our lives and community. Join us in moments of intercession where we seek God's guidance and blessings for our church, families, and beyond. Discover the strength that comes from building a culture of prayer and connection with the divine."
   }
-  ,{
-    heading:'beautiful girl',
-    description:'Beautiful Girl is a ministry for women, in every stage of life, to grow closer to Jesus and to each other.'
-  }
-  ,{
-    heading:'Bold Men',
-    description:'Bold Men is a ministry for men, helping them lead and become the man of God they were made to be!'
-  }
-  ,{
-    heading:'Blessed Marriage',
-    description:'Blessed Marriage is a biblically-centered ministry that is dedicated to helping couples understand God’s purpose for marriage and equipping them in the pursuit of oneness.'
+  , 
+  {
+    heading: 'Community and Outreach',
+    description: "Explore the heart of our commitment to community and outreach. Understand the values that drive us to share God's love beyond the walls of our church. Join hands with us as we actively participate in community service and outreach programs, making a positive impact on the lives of those around us."
+  },
+  {
+    heading : "Missions and Global Outreach",
+    description : "Be part of something bigger! Discover our beliefs and commitment to global missions. Learn about the missionaries we support and engage with our global outreach initiatives. Together, let's spread the message of hope and salvation to every corner of the world."
+  },
+  {
+    heading : "Doctrine and Theology",
+    description: "Delve into the rich tapestry of our core doctrinal beliefs. Join discussions that explore the theological foundations shaping our faith. Gain a deeper understanding of the principles that guide our teachings, fostering a community rooted in a shared understanding of our faith." 
+  },
+  {
+    heading : "Stewardship and Generosity",
+    description : "Unlock the joy of stewardship and generosity in your life. Explore our beliefs on managing God's blessings and discover the fulfillment that comes from a generous heart. Join us in practicing financial stewardship and generosity as integral expressions of our faith."
+  },
+  {
+    heading : "Family and Marriage",
+    description : "Strengthen the fabric of your family and relationships through our shared values. Discover our beliefs on family values and the sanctity of marriage. Join conversations that nurture strong and faithful families, fostering an environment where love, respect, and faith thrive."
+  },
+  {
+    heading : "Spiritual Growth",
+    description : "Nurturing a deeper connection with God, emphasizing personal development, and fostering a continuous journey of spiritual maturity."
+  },
+  {
+    heading : "Social Justice",
+    description : "Advocating for fairness, equality, and addressing societal issues in alignment with our faith values, as we actively work towards a more just and compassionate world."
   }
 ]
   constructor() { }
--- src/app/Modules/Main-Church/what-we-believe/what-we-believe.component.ts
+++ src/app/Modules/Main-Church/what-we-believe/what-we-believe.component.ts
@@ -11,50 +11,75 @@ import { SharedModule } from '../../../Shared/Shared.module';
 export class WhatWeBelieveComponent implements OnInit {
   contents = [
     {
-      heading: 'Discipleship',
+      heading: 'The Bible',
       description: 
-      'This is your next step! Learn about the mission, values, and vision of Gospel Centre. This is a 1-time class that will answer any questions you may have, introduce you to key leaders, and give you a tour. Be ALL IN for your church and get plugged in now!'
+      'We believe in the divine inspiration and authority of the Bible, consisting of the Old and New Testaments. It is the infallible and authoritative guide for faith and practice, revealing the will of God for salvation and the Christian life.'
     },
     {
-      heading: 'Kids and Teens',
-      description: 'Kids are important to us at Gospel Centre! Find out about our ministries for little g’s (birth-kindergarten), gKids (1st-5th grade) and youth group for students in grades 6 through 12'
-    }, 
+      heading : "God",
+      description : "We believe in one God, who exists eternally in three persons: Father, Son, and Holy Spirit. God is the creator of all things, infinitely perfect and eternally existing in love, holiness, and justice."
+    },
     {
-      heading: 'Worship and Music',
-      description: `
-        Embrace the transformative power of worship in our community. Join our gatherings to experience the joy and connection that comes from praising God through music. Dive into the heart of our worship values and understand the significance of music in our spiritual journey. Come, let's lift our voices together!
-      `}
-    , 
+      heading : "Jesus Christ",
+      description : "We believe in the deity of Jesus Christ, the Son of God, who became incarnate, lived a sinless life, performed miracles, and atoned for sin through His death on the cross. We affirm His bodily resurrection and ascension, and His ongoing intercession for believers."
+    },
     {
-      heading: 'Prayer and Intercession',
-      description: "Engage in the spiritual discipline of prayer as we believe in its profound impact on our lives and community. Join us in moments of intercession where we seek God's guidance and blessings for our church, families, and beyond. Discover the strength that comes from building a culture of prayer and connection with the divine."
+      heading : "Fall of Man",
+      description : "We believe that humanity, created in the image of God, fell into sin through disobedience. As a result, all individuals are born with a sinful nature, separated from God and in need of salvation."
+    },
+    {
+      heading : "The Salvation of Man",
+      description : "We believe in the salvation of all who repent and believe in Jesus Christ as Lord and Savior. This salvation is a gift of God's grace, received through faith, and is characterized by regeneration, justification, and sanctification."
+    },
+    {
+      heading : "Sanctification",
+      description : "We believe in the ongoing process of sanctification, where believers are set apart for God's purposes and empowered by the Holy Spirit to grow in holiness and Christlikeness."
+    },
+    {
+      heading : "The Return of Christ",
+      description : "We believe in the imminent return of Jesus Christ, who will return in glory to judge the living and the dead. This event will culminate in the establishment of God's eternal kingdom."
+    },
+    {
+      heading : "The Church",
+      description : "We believe in the Church as the body of Christ, consisting of all believers. It is called to worship, fellowship, and witness, and is empowered by the Holy Spirit for ministry and mission."
+    },
+    {
+      heading : "The Holy Spirit",
+      description : "We believe in the person and work of the Holy Spirit, who convicts of sin, regenerates, indwells, and empowers believers for godly living and effective service."
+    },
+    {
+      heading : "The Gifts of the Spirit",
+      description : "We believe in the gifts of the Holy Spirit, as outlined in the New Testament, which are given to believers for the building up of the Church and the advancement of God's kingdom."
+    },
+    {
+      heading : "Healing",
+      description : "We believe in divine healing as a provision of God's grace, both for physical and spiritual restoration. It is received through faith and prayer in accordance with God's will."
+    },
+    {
+      heading : "All People",
+      description : "We believe in the dignity and worth of all people, created in the image of God. We affirm the universal need for salvation and the call to share the Gospel with every person."
+    },
+    {
+      heading : "Marriage",
+      description : "We believe in the sanctity of marriage, defined as a covenant between one man and one woman. It reflects the biblical imagery of Christ and the Church."
+    },
+    {
+      heading : "Sexuality",
+      description : "We believe in the biblical understanding of sexuality, affirming that sexual intimacy is designed by God for marriage between a man and a woman. We uphold the biblical teachings on purity and holiness in all areas of life."
     }
-    , 
-    {
-      heading: 'Community and Outreach',
-      description: "Explore the heart of our commitment to community and outreach. Understand the values that drive us to share God's love beyond the walls of our church. Join hands with us as we actively participate in community service and outreach programs, making a positive impact on the lives of those around us."
-    },
-    {
-      heading : "Missions and Global Outreach",
-      description : "Be part of something bigger! Discover our beliefs and commitment to global missions. Learn about the missionaries we support and engage with our global outreach initiatives. Together, let's spread the message of hope and salvation to every corner of the world."
-    },
-    {
-      heading : "Doctrine and Theology",
-      description: "Delve into the rich tapestry of our core doctrinal beliefs. Join discussions that explore the theological foundations shaping our faith. Gain a deeper understanding of the principles that guide our teachings, fostering a community rooted in a shared understanding of our faith." 
-    },
-    {
-      heading : "Family and Marriage",
-      description : "Strengthen the fabric of your family and relationships through our shared values. Discover our beliefs on family values and the sanctity of marriage. Join conversations that nurture strong and faithful families, fostering an environment where love, respect, and faith thrive."
-    }
-
-    // , 
-    // {
-    //   heading: 'Blessed Marriage',
-    //   description: 'Blessed Marriage is a biblically-centered ministry that is dedicated to helping couples understand God’s purpose for marriage and equipping them in the pursuit of oneness.'
-    // }
   ]
-  values = ['Whatever It Takes', 'Show Our Passion', 'We Bleed GC', 'Never Stop Growing', 'Honor Up, Down, and All Around',
-    'Church Should Be Fun', 'Make It Attractive', 'Reject Good for Great', 'Lead From the Front', 'We Get to Give']
+  values = [
+    "Faith in Action",
+    "Love Unconditionally",
+    "Unity in Diversity",
+    "Integrity Above All",
+    "Service with Purpose",
+    "Generosity in Giving",
+    "Respect for All",
+    "Excellence in Everything",
+    "Inclusivity Always",
+    "Empowerment for All"
+  ];
   constructor() { }
 
   ngOnInit() {
