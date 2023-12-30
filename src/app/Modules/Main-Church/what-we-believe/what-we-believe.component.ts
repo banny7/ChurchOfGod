9d42c6441f93f41bceade3d7850164e53cbb63d2
acksa123 <acksa.varghese@logicplum.com>
2023-12-30 09:24:51 +0530

    Connect Our Beliefs section started

--- src/app/Modules/Main-Church/what-we-believe/what-we-believe.component.ts
+++ src/app/Modules/Main-Church/what-we-believe/what-we-believe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { SharedModule } from '../../../Shared/Shared.module';
 
 @Component({
@@ -14,5 +14,22 @@ export class WhatWeBelieveComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  translationLeft = 0;
+  translationRight = 0;
+  translationSpeed = 0.2;
+  
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    // Update translations based on scroll position and speed
+    this.translationLeft = window.scrollY * this.translationSpeed;
+    this.translationRight = -window.scrollY * this.translationSpeed; // Use a negative value for right translation
+  }
+  
+  getTranslationLeft(): string {
+    return `translateX(${this.translationLeft}px)`;
+  }
+  
+  getTranslationRight(): string {
+    return `translateX(${this.translationRight}px)`;
+  }
 }
